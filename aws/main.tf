3c501b9b7a9b3e0b6207c7af777e18ae93ef9bf8
Ubuntu <ubuntu@ip-172-31-45-228.ap-southeast-2.compute.internal>
2022-12-15 08:53:13 +0000

    Revert "modified main and variables file"
    This reverts commit cf3dc062f79309dbef07dfff6a7617baf470954d.

--- aws/main.tf
+++ aws/main.tf
@@ -1,26 +1,27 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-  required_version = ">= 1.2.0"
+required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 4.16"
+}
+}
+        required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region = "ap-southeast-2"
+region = "us-east-1"
 }
 
 locals {
-	instances = {"rk1":"ami-06bb074d1e196d0d4","rk2":"ami-0df609f69029c9bdb","rk3":"ami-08cd0547bf85331e7","rk4":"ami-0df609f69029c9bdb"}
+	instances = {"Devendra":"ami-0b0dcb5067f052a63","Braj":"ami-08c40ec9ead489470","Abrar":"ami-08c40ec9ead489470","Nimo":"ami-0b0dcb5067f052a63"}
 }
 
-resource "aws_instance" "my_ec2_test" {
-  for_each      = local.instances
-  ami           = each.value
-  instance_type = "t2.micro"
-  tags = {
-    Name = each.key 
+resource "aws_instance" "aws_ec2_test" {
+        for_each = local.instances
+        ami = each.value
+        instance_type = "t2.micro"
+        tags = {
+     Name = each.key
   }
 }
+
